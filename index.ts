504ec2d7863e95af70bc39f8faf48eb56688f922
kaliSaada <khalil062010@gmail.com>
2021-02-24 23:15:08 -0300

    feat: config eslint

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
-import express from 'express';
+import express from 'express'
 import morgan from 'morgan';
 
 const app = express();
 
 const PORT = 3000;
 
-app.use(morgan('combined'))
+app.use(
+  morgan('combined'))
+
 
 app.get('/', (req, res) => res.send('Test'));
 
