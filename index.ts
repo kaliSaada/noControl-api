31a8b552f42be746e54875cc818ec3784de2fa66
kaliSaada <khalil062010@gmail.com>
2021-02-24 21:53:10 -0300

    feat: config nodemon for autocompile

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import morgan from 'morgan'
 
 const app = express();
 
-const PORT = 8000;
+const PORT = 3000;
+
+app.use(morgan('combined'))
 
 app.get('/', (req, res) => res.send('Test'));
 
